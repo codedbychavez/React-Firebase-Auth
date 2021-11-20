d449281a7d13815d1408fb5527ce1a90bdf4b50a
Chavez Harris <74829200+softiosolutions@users.noreply.github.com>
2021-11-20 13:11:05 -0400

    added firebase config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSHV0bo4IxZZ-QDUadthRCdAT8snTEQP4",
+  authDomain: "react-firebase-auth-f1e6d.firebaseapp.com",
+  projectId: "react-firebase-auth-f1e6d",
+  storageBucket: "react-firebase-auth-f1e6d.appspot.com",
+  messagingSenderId: "66112923052",
+  appId: "1:66112923052:web:bb2cdb36180e020ebb8609"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
